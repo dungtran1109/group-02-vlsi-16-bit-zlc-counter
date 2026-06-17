3460787bf8464d95f4b7c4d3348543ab3a807a81
nguyencamuyen23-star <nguyencamuyen23@gmail.com>
2026-06-17 11:06:57 +0700

    Update LZC16 testbench

--- frontend/xschem/tb_lzc16.v
+++ frontend/xschem/tb_lzc16.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 
 module tb_lzc16;
-    reg [15:0] A;
-    wire [4:0] Y;
 
-    // Map các bit của Vector A và Y vào từng chân lẻ của netlist
+    reg  [15:0] A;
+    wire [4:0]  Y;
+    integer i;
+
+    // DUT
     lzc16 uut (
         .Y4(Y[4]), .Y3(Y[3]), .Y2(Y[2]), .Y1(Y[1]), .Y0(Y[0]),
         .A15(A[15]), .A14(A[14]), .A13(A[13]), .A12(A[12]),
@@ -17,14 +19,23 @@ module tb_lzc16;
         $dumpfile("lzc16_wave.vcd");
         $dumpvars(0, tb_lzc16);
 
-        $monitor("Time=%0t | A=%b | Zeros Y=%d", $time, A, Y);
+        $display("======================================");
+        $display("     TEST LEADING ZERO COUNTER");
+        $display("======================================");
 
-        // Test các trường hợp
-        A = 16'b0001_0110_0010_0000; #10; // Đề bài (3 số 0)
-        A = 16'b1000_0000_0000_0000; #10; // 0 số 0
-        A = 16'b0000_0000_0000_0000; #10; // 16 số 0
-        A = 16'b0000_0000_1111_1111; #10; // 8 số 0
+        // Test từ 0 leading zero đến 15 leading zero
+        for(i = 15; i >= 0; i = i - 1) begin
+            A = 16'b1 << i;
+            #10;
+            $display("A = %b , Y = %d", A, Y);
+        end
+
+        // Test tất cả bằng 0
+        A = 16'b0000_0000_0000_0000;
+        #10;
+        $display("A = %b , Y = %d", A, Y);
 
         $finish;
     end
+
 endmodule
